b9bd583c775b384de2272810b942ace8f7cbb445
vpena@inglosa.hn <vpena@inglosa.hn>
2025-09-12 08:51:17 -0600

    Implement email sending functionality in correo_averia_descuento_aviso.php

--- www/correo_averia_descuento_aviso.php
+++ www/correo_averia_descuento_aviso.php
@@ -6,7 +6,10 @@ $app_enviar_email=true;//susttituir por app_enviar_email despues
 if ($app_enviar_email==true) {
     require_once ('include/correo.php');
 
-    echo 'email';
+    $cuerpohtml="<h1>hola mundo</h1>";
+    $cuerpo_sinhtml=strip_tags($cuerpohtml);
+
+    enviar_correo('alexander.v211111@gmail.com','aprobacion descuento',$cuerpohtml,$cuerpo_sinhtml,true);
 }
 
 ?>
