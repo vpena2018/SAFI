619f10a20daf6bbdc9e9fdba845687aac3328b7d
David Velasquez <alexander.v211111@gmail.com>
2026-06-03 15:25:41 -0600

    GUI

--- www/traslados/index.php
+++ www/traslados/index.php
@@ -12,6 +12,265 @@ require_once ('../include/config.php' );
 			$conn->set_charset("utf8");
 	} else { echo 'Server Error [101]">'; exit; }
 
+	function fraccion($valor){
+    $salida=$valor;
+    $pieces = explode("/", $valor);
+	if (is_array($pieces)){
+        if (count($pieces)>1) {         
+                $salida="<sup>".$pieces[0]."</sup>&frasl;<sub>".$pieces[1]."</sub>"  ;           
+        }		
+	}  
+     
+
+    return $salida;
+}
+
+function campo_combustible($nombre,$valor,$adicional=""){
+    $salida='';
+    $lineas  = array('E','1/16','1/8','3/16','1/4','5/16','3/8','7/16','1/2','9/16','5/8','11/16','3/4','13/16','7/8','15/16','F');
+    
+    $salida.=' <div class="btn-group btn-group-toggle mb-2 flex-wrap" data-toggle="buttons" >';
+       
+    $i=0;
+    foreach ($lineas as  $value) {
+        $i++;
+        if ($value==$valor) {$selected=" checked"; $active=" active";} else {$selected=""; $active="";}
+        $salida.=' <label class=" btn btn-outline-secondary btn-sm '.$active.'">
+        <input type="radio" class="" name="'.$nombre.'"   value="'.$value.'"  '.$selected.' '.$adicional.' > 
+        '.fraccion($value).'</label>'; //id="'.$nombre.$i.'"
+        // $salida.='<input class="form-check-input" type="radio" name="'.$nombre.'" id="'.$nombre.$i.'" value="'.$value.'" '.$selected.' required>
+        // <label class="form-check-label btn btn-outline-primary btn-sm" for="'.$nombre.$i.'" '.$active.'>'.fraccion($value).'</label>';
+        
+
+    }
+    $salida.='</div>';
+    return $salida;
+}
+
+	function campo($nombre,$etiqueta,$tipo,$valor,$class="",$adicional="",$valor2="",$valor_etiqueta="",$numero="") {
+	$salida="";
+	$salida_end="";
+	 // autocomplete="off"  class='form-control-plaintext' readonly
+
+	if ($etiqueta!="") { 
+ 		$salida = '<div class="form-label-group">';
+ 		$salida_end='<label for="'.$nombre.'">'.$etiqueta.'</label></div>';
+    }
+
+	switch ($tipo) {
+
+		case "label":    
+			$salida.= '<input  type="text"  id="'.$nombre.'" name="'.$nombre.'" value="'.$valor.'" placeholder="" class="form-control-plaintext '.$class.'" '.$adicional.' readonly >';
+		    break;
+        
+		case "labelb":
+
+			$salida = '<div class="form-label-group">';
+			$salida.= '<span class="label-label" style="color:#8e613e;">'.$etiqueta.'</span>';
+
+			if ($valor2<>'') {
+				$salida.= ' <a href="#" onclick="'.$valor2.' return false;" class="label-icon"><i class="fa fa-edit"></i></a>';
+			}
+
+			$salida.= '<br><span class="label-texto" style="color:#000;">'.$valor.'</span>';
+			$salida_end='</div>';
+			break;
+
+		case "text":   
+			$salida.= '<input type="'.$tipo.'" id="'.$nombre.'" name="'.$nombre.'" value="'.$valor.'" placeholder="" class="form-control '.$class.'"  '.$adicional.' autocomplete="off">';
+	    	break;
+
+	    case "password":   
+			$salida.= '<input type="'.$tipo.'" id="'.$nombre.'" name="'.$nombre.'" value="'.$valor.'" placeholder="" class="form-control '.$class.'"  '.$adicional.'>';
+	    	break;
+
+	    case "number":   
+			$salida.= '<input type="'.$tipo.'" id="'.$nombre.'" name="'.$nombre.'" value="'.$valor.'" placeholder="" class="form-control '.$class.'"  '.$adicional.'  autocomplete="off">';
+	    	break;
+
+	    case "email":   
+			$salida.= '<input type="'.$tipo.'" id="'.$nombre.'" name="'.$nombre.'" value="'.$valor.'" placeholder="" class="form-control '.$class.'"  '.$adicional.'  autocomplete="off">';
+	    	break;
+
+	    case "password":   
+			$salida.= '<input type="'.$tipo.'" id="'.$nombre.'" name="'.$nombre.'" value="'.$valor.'" placeholder="" class="form-control '.$class.'"  '.$adicional.'>';
+	    	break;
+	
+		case "hidden":   
+			$salida = '<input id="'.$nombre.'" name="'.$nombre.'" value="'.$valor.'"  type="'.$tipo.'"  '.$adicional.' />';
+	    	$salida_end="";
+	    	break; 
+
+	    case "textarea":
+	    	$salida.= '<textarea id="'.$nombre.'" name="'.$nombre.'" spellcheck="false" class="form-control '.$class.'" '.$adicional.'>'.$valor.'</textarea>';
+	      	break; 
+
+	    case "select":  
+	    	$salida.= '<select id="'.$nombre.'" name="'.$nombre.'" class="form-control '.$class.'" '.$adicional.'>'.$valor.'</select>';
+	    	break; 
+         
+            case "checkboxCustom":  
+
+            $checked = $valor ? "checked" : "";
+            $value_attr = "1"; // Valor que se enviará si el checkbox está marcado
+            $hidden_value = "0"; // Valor que se enviará si el checkbox no está marcado
+
+
+            $salida = '<div class="form-group"><div class="custom-control custom-checkbox">';
+            $salida .= '<input type="hidden" name="' . $nombre . '" value="' . $hidden_value . '">';
+            $salida .= '<input id="' . $nombre . '" name="' . $nombre . '" value="' . $value_attr . '" type="checkbox" class="custom-control-input ' . $class . '" ' . $checked . ' ' . $adicional . ' />';
+            $salida_end = ' <label class="custom-control-label" for="' . $nombre . '">' . $etiqueta . '</label></div></div>';
+		    break;
+
+	    case "checkbox":  
+      		$salida ='<div class="form-group"><div class="custom-control custom-checkbox ">';
+			$salida .= '<input id="'.$nombre.'" name="'.$nombre.'" value="'.$valor.'"  type="checkbox" class="custom-control-input '.$class.'" '.$adicional.' />';
+		    $salida_end=' <label class="custom-control-label" for="'.$nombre.'">'.$etiqueta.'</label></div></div>';
+		    break;
+
+		case "checkbox2": 
+		  	$salida ='<div class="form-group"> <div class="custom-control custom-switch">';
+			$salida .= '<input id="'.$nombre.'" name="'.$nombre.'" value="'.$valor.'"  type="checkbox" class="custom-control-input '.$class.'" '.$adicional.' />';
+		    $salida_end='<label class="custom-control-label" for="'.$nombre.'">'.$etiqueta.'</label></div></div>';
+		    break;
+
+		case "radio":  
+   			$salida ='<div class="form-group"><div class="custom-control custom-radio">';
+			$salida .= '<input id="'.$nombre.'" name="'.$nombre.'" value="'.$valor.'"  type="'.$tipo.'" class="custom-control-input '.$class.'" '.$adicional.' />';
+		    $salida_end='<label class="custom-control-label" for="'.$nombre.'">'.$etiqueta.'</label></div></div>';
+		    break;
+
+	// 	case "date":  
+    //   // $salida.= '<script>$(function() {$( "#'.$nombre.'" ).datepicker({showOn: "both",buttonImage: "images/calendar.gif",buttonImageOnly: true,changeMonth: true,changeYear: true,dateFormat: "'.$_SESSION['formato_fecha_jquery'].'"}); });</script>';
+    //   // $salida .= '<input id="'.$nombre.'" name="'.$nombre.'" value="'.$valor.'"  type="text" '.$adicional.' />';
+    //      $salida .= '<input id="'.$nombre.'" name="'.$nombre.'" value="'.$valor.'" data-date-format="'.$_SESSION['formato_fecha'].'" type="text" class="form-control '.$class.'" '.$adicional.' />';         
+    //      $salida .=  "<script> 	$('#$nombre').datepicker();	</script> ";
+	// 	 break;
+
+    //      case "hora":  
+    //         // $salida.= '<script>$(function() {$( "#'.$nombre.'" ).datepicker({showOn: "both",buttonImage: "images/calendar.gif",buttonImageOnly: true,changeMonth: true,changeYear: true,dateFormat: "'.$_SESSION['formato_fecha_jquery'].'"}); });</script>';
+    //         // $salida .= '<input id="'.$nombre.'" name="'.$nombre.'" value="'.$valor.'"  type="text" '.$adicional.' />';
+    //            $salida .= '<input id="'.$nombre.'" name="'.$nombre.'" value="'.$valor.'" data-date-format="'.$_SESSION['formato_fecha'].'" type="text" class="form-control '.$class.'" '.$adicional.' />';         
+    //            $salida .=  "<script> 	$('#$nombre').datepicker();	</script> ";
+                                  
+         
+          
+    //  	 break;
+
+    case "date":   
+        $salida.= '<input type="'.$tipo.'" id="'.$nombre.'" name="'.$nombre.'" value="'.$valor.'" " class="form-control '.$class.'"  '.$adicional.' autocomplete="off">';
+        break;
+
+        case "time":   
+			$salida.= '<input type="'.$tipo.'" id="'.$nombre.'" name="'.$nombre.'" value="'.$valor.'"  class="form-control '.$class.'"  '.$adicional.' autocomplete="off">';
+	    	break;
+
+        case "datetime-local":   
+        $salida.= '<input type="'.$tipo.'" id="'.$nombre.'" name="'.$nombre.'" value="'.$valor.'" " class="form-control '.$class.'"  '.$adicional.' autocomplete="off">';
+        break;
+
+		case "boton":  //falta  
+			$salida .= '<input id="'.$nombre.'" name="'.$nombre.'" value="'.$valor.'"  type="'.$tipo.'" class="form-control '.$class.'" '.$adicional.' />';
+		    break; 
+
+
+		case "select2":
+	
+		  /*$lg=',language: "es"' ; 
+		  $salida = '<div class="form-group"><label class="outside-label">'.$etiqueta.'</label>';
+	      $salida.= '<select id="'.$nombre.'" name="'.$nombre.'" class="form-control select2 '.$class.'" style="width: 100%;" '.$adicional.'>'.$valor.'</select>';
+	      $salida.= '<script>$(document).ready(function() {$("#'.$nombre.'").select2( {theme: "classic" '.$lg.'   }); });    </script>';
+		  $salida_end='</div>';
+	      break;*/
+                      //evento change para ventas_mant.php
+            if ($nombre == 'id_estado') {
+            $evento_estado = ' onchange="toggleClientePorEstado(this)" ';
+            } else {
+                $evento_estado = '';
+            }
+
+            $lg = ',language: "es"';
+
+            $salida = '<div class="form-group">';
+            if ($etiqueta != '') {
+                $salida .= '<label class="outside-label">'.$etiqueta.'</label>';
+            }
+
+            $salida .= '
+                <select 
+                    id="'.$nombre.'" 
+                    name="'.$nombre.'" 
+                    class="form-control select2 '.$class.'" 
+                    style="width:100%;" 
+                    '.$adicional.'
+                    '.$evento_estado.'
+                >
+                    '.$valor.'
+                </select>';
+
+            $salida .= '
+                <script>
+                $(document).ready(function() {
+                    $("#'.$nombre.'").select2({
+                        theme: "classic" '.$lg.'
+                    });
+                });
+                </script>';
+
+            $salida_end = '</div>';
+            break;  
+
+		case "select2ajax":
+            $nombre_id=$nombre;
+            if (substr($nombre_id, -2)=="[]") {
+                $nombre_id =substr($nombre_id, 0, -2).$numero;
+            }
+		  $lg=',language: "es"' ; 
+		  $salida = '<div class="form-group"><label class="outside-label">'.$etiqueta.'</label>';
+	      $salida.= '<select id="'.$nombre_id.'" name="'.$nombre.'" class="form-control select2 '.$class.'" style="width: 100%;" '.$adicional.'>';
+	      if(trim($valor_etiqueta)<>""){
+              $salida .= '<option value="'.$valor.'" selected="selected">'.$valor_etiqueta.'</option>';
+          }
+          $salida.= '</select>';
+
+          $salida.= '<script>$(document).ready(function() {$("#'.$nombre_id.'").select2( {theme: "classic" '.$lg;
+		  $salida.= ", ajax: {
+		    url: '".$valor2."',
+		    
+		    dataType: 'json',
+		    delay: 250,
+		    data: function (params) {
+		      return {
+		        q: params.term, 
+		        page: params.page
+		      };
+		    },
+		    processResults: function (data) {	          
+		            return {
+		                
+		                results: data.results
+		            };
+		        },
+		        
+		    cache: false
+		  },
+		 
+		  minimumInputLength: 3, });
+		   }); </script>"; 
+		  $salida_end='</div>';
+
+		break;  
+
+	}			
+
+ 
+
+	$salida .= $salida_end;      
+
+	return $salida;
+
+}
+
 
 	//funciones de BD//
 	function sql_select($sql,$param="",$sql2="") {
@@ -117,15 +376,12 @@ $txt_mensaje="";
     
 	<link href="css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link href="js/sweetalert2/sweetalert2.min.css" rel="stylesheet">
+	<link href="css/app2.css" rel="stylesheet">
 	<link href="css/index.css" rel="stylesheet">
+	
 	<link href="js/helloweek/styles/hello.week.min.css" rel="stylesheet" />
 	<link href="js/helloweek/styles/hello.week.theme.min.css" rel="stylesheet" />
-	<link href="js/helloweek/public/styles/main.css" rel="stylesheet" />
-	<script>
 
-
-
-	</script>
   </head>
   <body>
 
@@ -204,56 +460,92 @@ $txt_mensaje="";
 
 
                 <div id="resultadoBusqueda"
-                     class="mt-4" style="display: none;">
+                     class="mt-4" style="display:block;">
 
                     <div class="row mb-3">
 
                         <div class="col-md-3">
-                            <small class="outside-label" style="color:#8e613e;">Numero</small>
-                            <div id="lblNumero"></div>
+							<?php $numero_traslado='58421' ; echo campo("numero_trasladolbl","Numero",'labelb',$numero_traslado,'',' ');?>
                         </div>
+						
 
-                        <div class="col-md-3">
-                            <small class="outside-label" style="color:#8e613e;">Fecha</small>
-                            <div id="lblFecha"></div>
-                        </div>
+						<div class="col-md-3">
+							<?php
+								$fecha = '03/06/2026';
+								echo campo("fecha_lbl", "Fecha", "labelb", $fecha, '', ' ');
+							?>
+						</div>
 
-                        <div class="col-md-3">
-                            <small class="outside-label" style="color:#8e613e;">Tienda</small>
-                            <div id="lblTienda"></div>
-                        </div>
+						<div class="col-md-3">
+							<?php
+								$tienda = 'Tienda Central';
+								echo campo("tienda_lbl", "Tienda", "labelb", $tienda, '', ' ');
+							?>
+						</div>
 
-                        <div class="col-md-3">
-                            <small class="outside-label" style="color:#8e613e;">Estado</small>
-                            <div id="lblEstado"></div>
-                        </div>
+						<div class="col-md-3">
+							<?php
+								$estado = 'Pendiente';
+								echo campo("estado_lbl", "Estado", "labelb", $estado, '', ' ');
+							?>
+						</div>
 
                     </div>
 
 
-                    <div class="row mb-3">
+					<div class="row mb-3">
 
-                        <div class="col-12">
-                            <small class="outside-label" style="color:#8e613e;">Vehiculo</small>
-                            <div id="lblVehiculo"></div>
-                        </div>
+						<div class="col-12">
+							<?php
+								$vehiculo = 'EA-03946 MITSUBISHI MONTERO SPORT 4X4 2024 CAMIONETA COLOR BLANCO DIAMANTE DIESEL HDW3633';
+								echo campo("vehiculo_lbl", "Vehiculo", "labelb", $vehiculo, '', ' ');
+							?>
+						</div>
 
-                    </div>
+					</div>
 
 
-                    <div class="row">
+					<div class="row">
 
-                        <div class="col-md-6">
-                            <small class="outside-label" style="color:#8e613e;">Salida</small>
-                            <div id="lblSalida"></div>
-                        </div>
+						<div class="col-md-6">
+							<?php
+								$salida = 'Hertz Tegucigalpa';
+								echo campo("salida_lbl", "Salida", "labelb", $salida, '', ' ');
+							?>
+						</div>
 
-                        <div class="col-md-6">
-                            <small class="outside-label" style="color:#8e613e;">Destino</small>
-                            <div id="lblDestino"></div>
-                        </div>
+						<div class="col-md-6">
+							<?php
+								$destino = 'Tienda Norte';
+								echo campo("destino_lbl", "Destino", "labelb", $destino, '', ' ');
+							?>
+						</div>
 
-                    </div>
+					</div>
+
+					<div class="row mb-2"> 
+								
+								<div class="col-md-4">   
+									<span class="outside-label">Combustible Salida</span>
+									<?php 	$combustible_salida = '3/8';	
+											$disable_combsalida = 'disabled';				
+										echo campo_combustible('combustible_salida',$combustible_salida,$disable_combsalida);       
+									?>              
+								</div>
+								
+
+								<div class="col-md-4 <?php echo $mostrar_entrada; ?>">  
+								<span class="outside-label">Combustible Entrada</span>
+									<?php 	$combustible_entrada = '3/8';	
+											$disable_combentrada = 'disabled';				
+										echo campo_combustible('combustible_entrada',$combustible_entrada,$disable_combentrada);
+									?>              
+								</div>
+
+								
+					</div>
+
+					
 
                 </div>
 
