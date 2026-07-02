74fb939e2e4ef52867e82aeb4a4b4f4b150560b1
vpena2018 <vpena@inglosa.hn>
2026-07-02 09:17:37 -0600

    Eliminar validación de permisos para el precio de venta en el contrato y simplificar la lógica de validación de precios

--- www/ventas_mant_contrato.php
+++ www/ventas_mant_contrato.php
@@ -1618,13 +1618,10 @@ if (!es_nulo($cid) && $genera_contrato == 1) {
     }
 
     //valida que el precio de venta no sea menor al precio minimo y que no sea mayor al precio maximo  
-    if (!tiene_permiso(193)){
-        $precio_venta=intval($_REQUEST['precio_venta'])+intval($_REQUEST['descuento_aplicado']);
-        if (!es_nulo($precio_venta) && !es_nulo($precio_minimo) && $precio_venta < $precio_minimo) {
-            $verror .= 'El precio de venta no puede ser menor al precio mínimo. ';
-        }
-    }
-
+    $precio_venta=intval($_REQUEST['precio_venta'])+intval($_REQUEST['descuento_aplicado']);
+    if (!es_nulo($precio_venta) && !es_nulo($precio_minimo) && $precio_venta < $precio_minimo) {
+       $verror .= 'El precio de venta no puede ser menor al precio mínimo. ';
+    }   
         
     if (!es_nulo($precio_venta) && !es_nulo($precio_maximo) && $precio_venta > $precio_maximo) {
         $verror .= 'El precio de venta no puede ser mayor al precio máximo. ';
