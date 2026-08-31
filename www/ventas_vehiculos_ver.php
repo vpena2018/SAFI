2a6e626009cecaef5580a42b1603fe7ccabb3558
vpena2018 <vpena@inglosa.hn>
2026-08-31 08:05:56 -0600

    Se agrego columna de Adjuntos Comprobante y recibos

--- www/ventas_vehiculos_ver.php
+++ www/ventas_vehiculos_ver.php
@@ -62,6 +62,8 @@ if ($accion=="1") {
     ,ventas_impuestos.nombre as elimpuesto 
 	 ,(SELECT COUNT(*) FROM ventas_fotos WHERE id_venta=ventas.id) fotos  
      ,DATEDIFF(NOW(), ventas.fecha_negociacion) AS dias_negociacion
+     ,ventas.foto as foto
+     ,ventas.foto_televentas as foto_televentas
         FROM ventas
         LEFT OUTER JOIN producto ON (ventas.id_producto=producto.id)        
         LEFT OUTER JOIN ventas_estado ON (ventas.id_estado=ventas_estado.id)
@@ -83,13 +85,14 @@ if ($accion=="1") {
                     <th>Numero</th>
                     <th>Fecha</th>
                     <th>Vehiculo</th>                    
-                    <th>Dias Negociacion</th>                    
+                    <th>Dias Negociacion</th>   
+                    <th>Adjuntos</th>   
                     <th>Precio Minimo</th>
                     <th>Precio Maximo</th>
                     <th>Estado</th>
                     <th>Impuesto</th>
                     <th>Creado</th> 
-                    <th>Fotos</th>        
+                    <th>Fotos Web</th>        
                 </tr>
             </thead>
             <tbody id="tablabody">
@@ -98,7 +101,9 @@ if ($accion=="1") {
 
             if ($result -> num_rows>=app_reg_por_pag) {$haymas=1;  }
             while ($row = $result -> fetch_assoc()) {
-
+                
+                $comprobantes = $row["foto"] ? 1 : 0;
+                $recibos = $row["foto_televentas"] ? 1 : 0;
                 $fotos = $row["fotos"];
 
                 // Definir color según el valor
@@ -109,6 +114,7 @@ if ($accion=="1") {
                 <td>'.formato_fecha_de_mysql($row["fecha"]).'</td>
                 <td>'.$row["codvehiculo"]. ' ' .$row["vehiculo"].'</td>
                 <td>'.$row["dias_negociacion"].'</td>
+                <td>'.$comprobantes+$recibos.'</td>
                 <td>'.$row["pminimo"].'</td>
                 <td>'.$row["pmaximo"].'</td>
                 <td>'.$row["elestado"].'</td>
